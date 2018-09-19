b5d6dff0a3568657b1b3d75e75d482f9f79c6a87
陈鹏 <chenpeng@yingyinglicai.com>
2018-09-19 21:12:19 +0800

    文件修改

--- Example/Pods/Target Support Files/Pods-example/Pods-example-umbrella.h
+++ Example/Pods/Target Support Files/Pods-example/Pods-example-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_page_manager_ExampleVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_page_manager_ExampleVersionString[];
+FOUNDATION_EXPORT double Pods_exampleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_exampleVersionString[];
 
--- Example/Pods/Target Support Files/Pods-page-manager_Example/Pods-page-manager_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_page_manager_Example : NSObject
-@end
-@implementation PodsDummy_Pods_page_manager_Example
-@end
